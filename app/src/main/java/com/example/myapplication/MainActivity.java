c419b8a342c4ae33a778b3e38e99d300684f6d37
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-02-18 10:59:37 +0300

    сохр1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "StartActivity";
+    private Integer counter = 5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreate");
+    }
+
+    public void krutki(View view) {
+        counter++;
+        TextView counterWiew = findViewById(R.id.txt_counter);
+        counterWiew.setText(counter);
+    }
+}
